306863f419b303efa0fef536fe99265b6dda0697
cbobrasil <cbobrasil@gmail.com>
2023-12-30 18:11:25 +0000

    adding fact games table

---
+++ tm/models/marts/fct_games.sql
@@ -0,0 +1,67 @@
+
+{{ config(materialized='table') }}
+
+with cte_cards as 
+  (
+  select    
+        game_id
+      , club_id      
+	    , card_type
+      , count(*) as event_qtt 
+  from    {{ ref('stg_game_events') }} 
+    where type = 'Cards'
+	group by
+        game_id
+      , club_id
+	    , card_type
+  )
+select
+      ge.game_id
+    , ge.competition_id
+    , ge.competition_type
+    , ge.season
+    , ge.round
+    , ge.date
+    , ge.home_club_id
+    , ge.away_club_id
+    , ge.home_club_goals
+    , ge.away_club_goals
+    , ge.home_club_position
+    , ge.away_club_position
+    , ge.home_club_manager_name
+    , ge.away_club_manager_name
+    , ge.stadium
+    , ge.attendance
+    , ge.referee
+    , ge.url
+    , ge.home_club_formation
+    , ge.away_club_formation
+    , ge.home_club_name
+    , ge.away_club_name
+    , ge.aggregate
+    , case 
+        when cte_cards_home.card_type = 'Yellow card'
+        then cte_cards_home.event_qtt 
+        else 0 
+      end as card_yellow_home_qtt
+    , case 
+        when cte_cards_home.card_type = 'Red card'
+        then cte_cards_home.event_qtt 
+        else 0
+      end as card_red_home_qtt
+    , case 
+        when cte_cards_away.card_type = 'Yellow card' 
+        then cte_cards_away.event_qtt 
+        else 0
+      end as card_yellow_away_qtt
+    , case 
+        when cte_cards_away.card_type = 'Red card'  
+        then cte_cards_away.event_qtt 
+        else 0
+      end as card_red_away_qtt
+from     
+  {{ ref('stg_games') }} as ge
+left join 
+  cte_cards as cte_cards_home on ge.game_id = cte_cards_home.game_id and  ge.home_club_id = cte_cards_home.club_id
+left join 
+  cte_cards as cte_cards_away on ge.game_id = cte_cards_away.game_id and  ge.away_club_id = cte_cards_away.club_id
