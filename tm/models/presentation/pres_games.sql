ac1d1afb5919240db1736571b8bff7b1c3b609f1
cbobrasil <cbobrasil@gmail.com>
2023-12-31 10:49:33 +0000

    adding new logic for presentation table

--- tm/models/presentation/pres_games.sql
+++ tm/models/presentation/pres_games.sql
@@ -6,17 +6,29 @@ with cte_cards as
   select    
         game_id
       , club_id      
-      , card_type
+      , card_color
       , count(*) as event_qtt 
   from    
-    {{ ref('stg_game_events') }} 
+    {{ ref('fct_games_events') }} 
   where
-    type = 'Cards'
+    type = 'Cards' and card_color is not null 
   group by
         game_id
       , club_id
-      , card_type
+      , card_color
   )
+, cte_goal_time as 
+(
+  select
+      game_id
+    , club_id 
+    , round(avg(minute),2) as time_avg
+  from {{ ref('fct_games_events')}} 
+    where type = 'Goals'
+    group by
+      game_id
+    , club_id 
+)
 
 select
       ge.game_id
@@ -26,24 +38,26 @@ select
     , ge.home_club_name
     , ge.away_club_name
     , ge.home_club_goals
+    , cgt_home.time_avg as home_club_time_avg_goals
     , ge.away_club_goals
+    , cgt_away.time_avg as away_club_time_avg_goals
     , case 
-        when cte_cards_home.card_type = 'Yellow card'
+        when cte_cards_home.card_color = 'Yellow'
         then cte_cards_home.event_qtt 
         else 0 
       end as card_yellow_home_qtt
     , case 
-        when cte_cards_home.card_type = 'Red card'
+        when cte_cards_home.card_color = 'Red'
         then cte_cards_home.event_qtt 
         else 0
       end as card_red_home_qtt
     , case 
-        when cte_cards_away.card_type = 'Yellow card' 
+        when cte_cards_away.card_color = 'Yellow' 
         then cte_cards_away.event_qtt 
         else 0
       end as card_yellow_away_qtt
     , case 
-        when cte_cards_away.card_type = 'Red card'  
+        when cte_cards_away.card_color = 'Red'  
         then cte_cards_away.event_qtt 
         else 0
       end as card_red_away_qtt
@@ -58,3 +72,7 @@ left join
     {{ ref('stg_club_games') }} as cg on ge.game_id = cg.game_id  and cg.club_id = ge.home_club_id
 left join 
     {{ ref('stg_competitions') }} as c on ge.competition_id = c.competition_id
+left join 
+    cte_goal_time as cgt_home on cgt_home.game_id = ge.game_id and cgt_home.club_id = ge.home_club_id 
+left join 
+    cte_goal_time as cgt_away on cgt_away.game_id = ge.game_id and cgt_away.club_id = ge.away_club_id
--- tm/models/staging/stg_game_events.sql
+++ tm/models/staging/stg_game_events.sql
@@ -10,10 +10,6 @@ select
     , club_id
     , player_id
     , substring(description, 2,8000)  as sub_type
-    , case
-        when description like '%Yellow%' or description like '%yellow%' then 	'Yellow card'
-        when  description like '%Red%' then 	'Red card'
-      else 'Not Card' end as card_type
     , player_in_id
     , player_assist_id
 from    
