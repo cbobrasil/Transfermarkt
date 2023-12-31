57a3d2d0bf5223245441dc772e1fe5bd0ba07067
cbobrasil <cbobrasil@gmail.com>
2023-12-31 03:00:42 +0000

    changes on fct

--- tm/models/marts/fct_games_events.sql
+++ tm/models/marts/fct_games_events.sql
@@ -2,32 +2,73 @@
 {{ config(materialized='table') }}
 
 select 
-    {{ dbt_utils.generate_surrogate_key(['game_event_id','sub_type_event']) }} as game_sub_type_event_id
-    , game_event_id
+      game_event_id
     , game_id
     , club_id  
     , player_id
     , minute
     , type
     , sub_type
-    , sub_type_event
-    --- COMPLETE WITH THE OTHER SUB_TYPES 
     , case 
-        when sub_type_event  like '1. Goal of the Season' then 'Goal'
-        when sub_type_event like '%Assist:%' then 'Assited Goal'
-        when sub_type_event like '%Assist%' then 'Assited Goal'
-        when sub_type_event like '%Assist: Corner%' then 'Assited Goal - Corner'
-        when sub_type_event like '%Assist: Cross%' then 'Assited Goal - Cross'
-        when sub_type_event like '%Assist: Fouled Played%' then 'Assited Goal - Fouled Played'
-        when sub_type_event like '%Assist: Free Kick%' then 'Assited Goal - Free Kick'
-        when sub_type_event like '%Assist: Handball%' then 'Assited Goal - Handball'
-        when sub_type_event like '%Assist: Header%' then 'Assited Goal - Header'
-        when sub_type_event like '%Assist: Pass%' then 'Assited Goal - Pass'
-        when sub_type_event like '%Assist: Shot on goal%' then 'Assited Goal - Shot on goal'
-        when sub_type_event like '%Assist: Without assist%' then 'Assited Goal - Without assist'
-        when sub_type_event like '%Assist: Without assist%' then 'Assited Goal - Without assist'
-        end as sub_type_event_2
---        when sub_type_event like '%Assist:%' then 'Assited Goal'
+        when type = 'Cards' and sub_type like '%Yellow%' then 'Yellow'
+        when  type = 'Cards' and sub_type like '%Yellow%' then 'Red'
+        else null 
+      end as card_color
+    , case 
+        when type = 'Cards' and sub_type like '%Foul%' then 'Foul'
+        when type = 'Cards' and sub_type like '%Mass confro%' then 'Mass Confrontation'
+        when type <> 'Cards' then null
+        else 'other reasons'
+      end as card_reason
+    , case
+        when type= 'Substitutions' and sub_type like '% Tactical%' then 'Tactical'
+        when type= 'Substitutions' and sub_type like '% Not reported%' then 'Not Reported'
+        when type <>'Substitutions' then null
+        else 'other reasons'
+      end as substitutions_reason
+    , case 
+        when  type = 'Goals' and sub_type like '%Counter attack goal%' then 'Counter attack'
+        when  type = 'Goals' and sub_type like '%Direct corner%' then 'Direct corner'
+        when  type = 'Goals' and sub_type like '%Solo run%' then 'Solo run'
+        when  type = 'Goals' and sub_type like '%Tap-in%' then 'Tap-in'
+        when  type = 'Goals' and sub_type like '%Solo run%' then 'Solo run'
+        when  type <>'Goals' then null
+        else 'Not defined'
+      end as goal_type
+    , case 
+        when type = 'Goals' and  sub_type like '%Assist:%' then true
+        when type <> 'Goals' then null 
+        else false
+      end as assisted_goal 
+    , case when type in('Cards','Substitutions') then null
+        when type = 'Goals' and sub_type like '%Corner%' then 'Corner'
+        when type = 'Goals' and sub_type like '%Cross%' then 'Cross'
+        when type = 'Goals' and sub_type like '%Free Kick%' or sub_type like '%Free kick%' then 'Free Kick'
+        when type = 'Goals' and sub_type like '%Handball%' then 'Handball'
+        when type = 'Goals' and sub_type like '%Header%' then 'Header'
+        when type = 'Goals' and sub_type like '%Pass%' then 'Pass'
+        when type = 'Goals' and sub_type like '%Shot on goal%' then 'Shot on goal'
+        when type = 'Goals' and sub_type like '%Without assist%' then 'Without assist'
+        when type = 'Goals' and sub_type like '%Goal-kick%' then 'Goal-kick'
+        when type = 'Goals' and sub_type like '%Long distance kick%' then 'Long distance kick'
+        when type = 'Goals' and sub_type like '%Direct free kick%' then 'Direct free kick'
+        else 'Assistence not defined'
+      end as assistence_type 
+    , case when sub_type like '%Right-footed%' then 'Right'
+        when sub_type like '%Left-footed%' then 'Left'
+        else null
+      end as goal_foot
+    , case 
+        when type = 'Goals' and sub_type like '%Penalty%' then true 
+        when type <>'Goals' then null
+        else false
+      end as goal_penalty
+    , case 
+        when type = 'Goals' and sub_type like '%Deflected shot on goal%' then true 
+        when type = 'Goals' and sub_type like '%Own-goal%' then true
+        when type <>'Goals' then null
+        else false
+      end as own_goal
     , player_in_id
     , player_assist_id
 from 
--- tm/models/staging/stg_game_events.sql
+++ tm/models/staging/stg_game_events.sql
@@ -1,8 +1,7 @@
 
 {{ config(materialized='table') }}
 
-with cte_game_events as 
-(select
+select
       game_event_id
     , date
     , game_id
@@ -19,24 +18,3 @@ with cte_game_events as
     , player_assist_id
 from    
     {{ ref('game_events') }}
-)
-, cte_array as 
-(select 
-   *
-  , string_to_array(sub_type,',') as sub_type_array
-from    
-    cte_game_events 
-)
-select    
-      game_event_id
-    , game_id
-    , club_id  
-    , player_id
-    , minute
-    , type
-    , sub_type
-    , unnest(sub_type_array) as sub_type_event 
-    , player_in_id
-    , player_assist_id
-from 
-  cte_array
